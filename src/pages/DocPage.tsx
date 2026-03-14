db48f2a0cc12567e82793151e43a7fb2033139c5
Jean-Christophe Oules <jcoules@gmail.com>
2026-03-14 06:18:58 +0100

    feat: iframe doc vers /doc-embed (même domaine, no-auth)

--- src/pages/DocPage.tsx
+++ src/pages/DocPage.tsx
@@ -2,7 +2,7 @@ export default function DocPage() {
   return (
     <div className="-m-4 lg:-m-8" style={{ height: 'calc(100vh - 1px)' }}>
       <iframe
-        src="https://doc.vertigocomhandicap.cloud"
+        src="/doc-embed"
         className="w-full h-full border-0"
         title="Assistant Doc"
         allow="camera; microphone"
